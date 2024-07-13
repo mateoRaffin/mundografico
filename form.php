3972ddb194e07d9fd1d2b153a0dd070e2f81c1f7
Mateo Nicolás Raffin <raffinmateonicolas@gmail.com>
2024-07-12 23:19:27 -0300

    arreglando form

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . "\r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n"; 
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$destinatario = 'raffinmateonicolas@gmail.com';
+$asunto = 'Este mail fue enviado desde la web';
+
+mail($destinatario, $asunto, $mensaje, $header);
+
+header('Location:index.html');
+
+?>
